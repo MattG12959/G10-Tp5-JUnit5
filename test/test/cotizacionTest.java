ec300915cad63500229b1c3e2368071b1f7228aa
NataliaDave <nataliadave@gmail.com>
2026-06-08 21:37:27 -0300

    cotizacionTest

---
+++ test/test/cotizacionTest.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package test;
+import org.junit.Assert;
+import modelo.ConversorMoneda;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Antonacci Matías - Dave Natalia - Zerdá Nehuen
+ */
+public class cotizacionTest {
+    @Before
+    public void antesDeCadaPrueba() {
+
+        System.out.println(
+                "\nPreparando prueba de cotización...");
+    }
+
+    @Test
+    public void testCotizarCero() {
+
+        System.out.println(
+                "Ejecutando: testCotizarCero");
+
+        System.out.println(
+                "Pesos: 180000");
+
+        System.out.println(
+                "Dólares: 0");
+
+        System.out.println(
+                "Se espera una ArithmeticException");
+
+        ConversorMoneda c =
+                new ConversorMoneda();
+
+        Assert.assertThrows(
+                ArithmeticException.class,
+                () -> {
+
+                    c.calcularCotizacion(
+                            180000,
+                            0);
+                });
+    }
+
+    @After
+    public void despuesDeCadaPrueba() {
+
+        System.out.println(
+                "Prueba finalizada.");
+    }
+}
+    
+
